c38c6b6390911fe2df2cb57e6056f677aa37ca83
Chamuel Patino <patinochamuel@gmail.com>
2025-06-16 14:15:34 -0400

    upated fmp_to_gbq_dag.py module, added pipeline.py module, added Dockerfile and upated docker-compose.yaml file

--- dags/fmp_to_gbq_dag.py
+++ dags/fmp_to_gbq_dag.py
@@ -3,34 +3,10 @@ from __future__ import annotations
 """Airflow DAG to ingets daily most active stoscks from FMP and load into BigQuery"""
 
 from datetime import datetime, timedelta
-import os
-
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
-from src.most_active import (
-    fetch_most_active_stocks,
-    process_data,
-    upload_to_bigquery,
-)
-
-def fetch_process_upload() -> None:
-    """Fetch most active stocks and load them into BIgQuery"""
-    project_id = os.getenv("GCP_PROJECT_ID", "woven-amulet-453222-r8")
-    dataset_id = os.getenv("BQ_DATASET", "stock_market_data")
-    table_id = os.getenv("BQ_TABLE", "most_active_stocks")
-
-    raw_data = fetch_most_active_stocks()
-    if not raw_data:
-        raise ValueError("Failed to fetch most active stocks")
-
-    df = process_data(raw_data)
-    if df is None or df.empty:
-        raise ValueError("No data returned from processing step")
-    
-    success = upload_to_bigquery(df, project_id, dataset_id, table_id)
-    if not success:
-        raise ValueError("BigQuery upload failed")
+from stock_market_analysis.pipeline import fetch_process_upload
 
 default_args = {
     "owner": "airflow",
